6287e47e97ce5afc44fd4a81c6f8e0e6d5c94bbc
hoangchung16-00 <hoangchung1687@gmail.com>
2026-02-01 14:25:11 +0700

    Make CORS initializer resilient if rack-cors missing

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,10 +1,16 @@
 # config/initializers/cors.rb
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins ENV.fetch('CORS_ORIGIN', 'http://localhost:4200')
-    resource '*',
-      headers: :any,
-      expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+begin
+  require 'rack/cors'
+rescue LoadError
+  Rails.logger.warn "rack-cors not available - skipping CORS middleware setup"
+else
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins ENV.fetch('CORS_ORIGIN', 'http://localhost:4200')
+      resource '*',
+        headers: :any,
+        expose: ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+        methods: %i[get post put patch delete options head]
+    end
   end
 end
